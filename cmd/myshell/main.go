cb75a84982da8f709645b08cdc055f0cba61bbc3
Mukhlas Rosyadi <mukhlas.rosyadi@dealls.com>
2024-05-26 22:28:54 +0700

    implement exit

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -7,6 +7,8 @@ import (
 	"io"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -28,9 +30,50 @@ func main() {
 			break
 		}
 
-		switch cmd := str[:len(str)-1]; cmd {
+		cmd := parseCmd(str[:len(str)-1])
+
+		switch cmd.name {
+		case "exit":
+			handleExit(cmd)
 		default:
-			fmt.Printf("%s: command not found\n", cmd)
+			fmt.Printf("%s: command not found\n", cmd.name)
 		}
 	}
+}
+
+type cmd struct {
+	name string
+	args []string
+}
+
+func parseCmd(str string) cmd {
+	// Split the string into words
+	words := strings.Fields(str)
+
+	// The first word is the command name
+	name := words[0]
+
+	// The rest of the words are the arguments
+	args := words[1:]
+
+	return cmd{name, args}
+}
+
+func handleExit(cmd cmd) {
+	if cmd.name != "exit" {
+		return
+	}
+
+	code := 0
+
+	if len(cmd.args) == 1 {
+		var err error
+		code, err = strconv.Atoi(cmd.args[0])
+		if err != nil {
+			fmt.Printf("exit: %s: numeric argument required\n", cmd.args[0])
+			code = 255
+		}
+	}
+
+	os.Exit(code)
 }
